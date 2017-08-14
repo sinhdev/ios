792875a0840b665c5da4d2884e07295bbf9ed1cd
Nguyễn Xuân Sinh <sinhnx.dev@gmail.com>
2017-08-14 17:05:32 +0700

    update with origin image function

--- TouchEventDemo/TouchEventDemo/ViewController.swift
+++ TouchEventDemo/TouchEventDemo/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgTransform = imgCover.transform
+        
         self.view.isUserInteractionEnabled = true
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapEvent)))
         self.view.addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(pinchGuestureAction)))
@@ -25,9 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func origineImage(_ sender: UIButton) {
+        imgCover.transform = imgTransform!
+    }
     func pinchGuestureAction(_ sender: UIPinchGestureRecognizer) {
         imgCover.transform = imgCover.transform.scaledBy(x: sender.scale, y: sender.scale)
     }
+    private var imgTransform:CGAffineTransform?
     
     func tapEvent(_ sender: UITapGestureRecognizer) {
         tapCount += 1
@@ -41,6 +47,8 @@ class ViewController: UIViewController {
         lblPosition.text = "x = \(t?.location(in: lblTouch).x ?? 0), y = \(t?.location(in: lblTouch).y ?? 0)"
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+        }
     }
 }
 
